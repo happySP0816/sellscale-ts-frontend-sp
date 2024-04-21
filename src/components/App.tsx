460c62e2bb8cff1c25246ddfd4667b16a0823a21
happySP0816 <happydeveloper0816@outlook.com>
2024-04-21 14:28:48 -0400

    Reply to add Clear Segment Modal

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -64,6 +64,7 @@ import SellscaleChat from './chat/sellscaleChat';
 import AutoSpitModal from '@modals/SegmentV2/AutoSpitModal';
 import SplitSegmentModal from '@modals/SegmentV2/SplitSegmentModal';
 import SegmentEditPrefilterModal from '@modals/SegmentV2/SegmentEditPrefilterModal';
+import ClearSegmentModal from '@modals/SegmentV2/ClearSegmentModal';
 
 export const socket = io(SOCKET_SERVICE_URL); //'http://localhost:3000');
 
@@ -229,6 +230,7 @@ export default function App() {
               autosplitsegment: AutoSpitModal,
               splitSegment: SplitSegmentModal,
               segmentprefilter: SegmentEditPrefilterModal,
+              clearsegment: ClearSegmentModal,
             }}
             modalProps={{
               closeOnClickOutside: false,
---
+++ src/components/modals/SegmentV2/ClearSegmentModal.tsx
@@ -0,0 +1,61 @@
+import { userTokenState } from '@atoms/userAtoms';
+import { API_URL } from '@constants/data';
+import { Button, Flex, Text } from '@mantine/core';
+import { ContextModalProps, closeAllModals } from '@mantine/modals';
+import { useState } from 'react';
+import { useRecoilValue } from 'recoil';
+
+interface DeleteSegment extends Record<string, unknown> {
+  showLoader: boolean;
+  segmentId: string;
+}
+
+export default function ClearSegmentModal({ innerProps }: ContextModalProps<DeleteSegment>) {
+  const userToken = useRecoilValue(userTokenState);
+  const [loading, setLoading] = useState(false);
+
+  const clearSegmentProspecst = async (showLoader: boolean, segmentId: string) => {
+    if (showLoader) {
+      setLoading(true);
+    }
+    fetch(`${API_URL}/segment/wipe_segment`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userToken}`,
+      },
+      body: JSON.stringify({
+        segment_id: segmentId,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {})
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <>
+      <Text color='gray' fw={500} size={'sm'} sx={{ display: 'flex', alignItems: 'center', gap: '3px' }}>
+        Are you sure you want to clear <span style={{ fontWeight: 500 }}>{'115 prospects'}</span>from this segment?
+      </Text>
+      <Flex gap={'md'} mt={'lg'}>
+        <Button fullWidth size='md' radius={'md'} variant='outline' color='gray' onClick={() => closeAllModals()}>
+          Cancel
+        </Button>
+        <Button
+          fullWidth
+          size='md'
+          color='red'
+          radius={'md'}
+          onClick={() => {
+            clearSegmentProspecst(innerProps.showLoader, innerProps.segmentId);
+          }}
+        >
+          Clear Segment
+        </Button>
+      </Flex>
+    </>
+  );
+}

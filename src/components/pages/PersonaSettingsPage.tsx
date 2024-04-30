65f613c65a00a5ca5a99ab52b23a8fa38eeb7d8c
happySP0816 <happydeveloper0816@outlook.com>
2024-04-29 18:02:13 -0700

    Clean Up Campaign Editing Page

--- src/components/pages/PersonaSettingsPage.tsx
+++ src/components/pages/PersonaSettingsPage.tsx
@@ -1,6 +1,6 @@
 import { currentProjectState } from "@atoms/personaAtoms";
 import { userDataState, userTokenState } from "@atoms/userAtoms";
-import { syncLocalStorage } from '@auth/core';
+import { syncLocalStorage } from "@auth/core";
 import PageFrame from "@common/PageFrame";
 import { PersonaBasicForm } from "@common/persona/PersonaBrain";
 import { PersonaCompanyInfo } from "@common/persona/settings/PersonaCompanyInfo";
@@ -24,7 +24,13 @@ import {
 import { openConfirmModal } from "@mantine/modals";
 import { showNotification } from "@mantine/notifications";
 import PageTitle from "@nav/PageTitle";
-import { IconBrain, IconBuilding, IconMessage, IconSettings, IconUser } from "@tabler/icons";
+import {
+  IconBrain,
+  IconBuilding,
+  IconMessage,
+  IconSettings,
+  IconUser,
+} from "@tabler/icons";
 import { activatePersona } from "@utils/requests/postPersonaActivation";
 import { deactivatePersona } from "@utils/requests/postPersonaDeactivation";
 import { useEffect, useRef, useState } from "react";
@@ -33,20 +39,20 @@ import { useRecoilState, useRecoilValue } from "recoil";
 export default function PersonaSettingsPage() {
   const userToken = useRecoilValue(userTokenState);
 
-  const [currentProject, setCurrentProject] =
-    useRecoilState(currentProjectState);
+  const [currentProject, setCurrentProject] = useRecoilState(
+    currentProjectState
+  );
   const [loading, setLoading] = useState(false);
 
   const [fetchedData, setFetchedData] = useState(false);
-    const [userData, setUserData] = useRecoilState(userDataState);
-
+  const [userData, setUserData] = useRecoilState(userDataState);
 
   useEffect(() => {
     if (!fetchedData) {
       syncLocalStorage(userToken, setUserData);
       setFetchedData(true);
     }
-  })
+  });
 
   if (currentProject == null) {
     return null;
@@ -56,15 +62,19 @@ export default function PersonaSettingsPage() {
     <PageFrame>
       <Container p="sm">
         <LoadingOverlay visible={loading} />
-        <Title order={4} align='center'>AI Brain Form</Title>
-        <Title order={1} align='center'>{currentProject.emoji} {currentProject.name}</Title>
+        <Title order={4} align="center">
+          Campaign Settings
+        </Title>
+        <Title order={1} align="center">
+          {currentProject.emoji} {currentProject.name}
+        </Title>
         <br />
-        <Tabs defaultValue="general" orientation="vertical" variant='pills'>
+        <Tabs defaultValue="general" orientation="vertical" variant="pills">
           <Tabs.List>
             <Tabs.Tab value="general" icon={<IconSettings size="0.8rem" />}>
-              Persona Info
+              General
             </Tabs.Tab>
-            <Tabs.Tab value="company" icon={<IconBuilding size="0.8rem" />}>
+            {/* <Tabs.Tab value="company" icon={<IconBuilding size="0.8rem" />}>
               Company Info
             </Tabs.Tab>
             <Tabs.Tab value="user" icon={<IconUser size="0.8rem" />}>
@@ -72,7 +82,7 @@ export default function PersonaSettingsPage() {
             </Tabs.Tab>
             <Tabs.Tab value="messaging" icon={<IconMessage size="0.8rem" />}>
               Messaging
-            </Tabs.Tab>
+            </Tabs.Tab> */}
           </Tabs.List>
 
           <Tabs.Panel value="general">
@@ -82,23 +92,22 @@ export default function PersonaSettingsPage() {
           </Tabs.Panel>
 
           <Tabs.Panel value="company">
-            <Flex ml='md'>
+            <Flex ml="md">
               <PersonaCompanyInfo />
             </Flex>
           </Tabs.Panel>
 
           <Tabs.Panel value="user">
-            <Flex ml='md'>
+            <Flex ml="md">
               <PersonaUserSettings />
             </Flex>
           </Tabs.Panel>
 
           <Tabs.Panel value="messaging">
-            <Flex ml='md'>
+            <Flex ml="md">
               <PersonaMessaging />
             </Flex>
           </Tabs.Panel>
-
         </Tabs>
       </Container>
     </PageFrame>

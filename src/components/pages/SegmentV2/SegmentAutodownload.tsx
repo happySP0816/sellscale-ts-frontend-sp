ec78ec53e01657b32bcdb44fd1bf40cc7c63ce16
happySP0816 <happydeveloper0816@outlook.com>
2024-04-30 19:56:42 -0700

    Segment Auto Download Updates

--- src/components/pages/SegmentV2/SegmentAutodownload.tsx
+++ src/components/pages/SegmentV2/SegmentAutodownload.tsx
@@ -1,3 +1,5 @@
+import { userTokenState } from "@atoms/userAtoms";
+import { API_URL } from "@constants/data";
 import {
   Box,
   Title,
@@ -11,19 +13,89 @@ import {
   TextInput,
   NumberInput,
   Button,
+  LoadingOverlay,
 } from "@mantine/core";
-import React from "react";
+import React, { useEffect } from "react";
+import { useRecoilValue } from "recoil";
 
 type PropsType = {
   segmentId: number | undefined | null;
-  segmentName: string | undefined;
-  segmentNumProspects: number | undefined;
   onDownloadHistoryClick: () => void;
 };
 
 export default function SegmentAutodownload(props: PropsType) {
+  const userToken = useRecoilValue(userTokenState);
+  const [numScrapesToRun, setNumScrapesToRun] = React.useState(1);
+  const [segment, setSegment] = React.useState({} as any);
+  const [fetchingSegment, setFetchingSegment] = React.useState(false);
+  const [hardcodedScrapePage, setHardcodedScrapePage] = React.useState(1);
+
+  const getSegment = (segmentId: number) => {
+    setFetchingSegment(true);
+    fetch(`${API_URL}/segment/${segmentId}`, {
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setSegment(data);
+      })
+      .finally(() => setFetchingSegment(false));
+  };
+
+  const toggleAutoDownload = () => {
+    fetch(`${API_URL}/segment/toggle_segment_auto_scrape`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userToken}`,
+      },
+      body: JSON.stringify({
+        segment_id: props.segmentId,
+      }),
+    }).then(() => props.segmentId && getSegment(props.segmentId));
+  };
+
+  const setCurrentScrapePage = () => {
+    fetch(`${API_URL}/segment/set_current_scrape_page`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userToken}`,
+      },
+      body: JSON.stringify({
+        segment_id: props.segmentId,
+        current_scrape_page: hardcodedScrapePage,
+      }),
+    }).then(() => props.segmentId && getSegment(props.segmentId));
+  };
+
+  const runScrapes = () => {
+    fetch(`${API_URL}/segment/run_scrapes`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userToken}`,
+      },
+      body: JSON.stringify({
+        segment_id: props.segmentId,
+        num_scrapes: numScrapesToRun,
+      }),
+    }).then(() => props.segmentId && getSegment(props.segmentId));
+  };
+
+  useEffect(() => {
+    if (props.segmentId) getSegment(props.segmentId);
+  }, [props.segmentId]);
+
+  const currentPage = segment?.current_scrape_page || 0;
+  const totalPages = Math.ceil(segment?.apollo_query?.num_results / 100) - 1;
+  const percentComplete = Math.round((currentPage / totalPages) * 100);
+
   return (
     <>
+      <LoadingOverlay visible={fetchingSegment} />
       <Box>
         <Title order={3}>Auto Download Segment</Title>
         <Text size="sm" color="gray">
@@ -38,35 +110,52 @@ export default function SegmentAutodownload(props: PropsType) {
         <Text fz="sm" weight={700}>
           Segment:
         </Text>
-        <Text fz="sm">{props.segmentName}</Text>
+        <Text fz="sm">{segment?.segment_title}</Text>
 
         <Text fz="sm" weight={700} mt="xs">
           Estimated # of prospects:
         </Text>
-        <Text fz="sm">
-          {props.segmentNumProspects?.toLocaleString()} prospects
-        </Text>
+        <Text fz="sm">{segment?.apollo_query?.num_results} prospects</Text>
       </Box>
 
       <Card withBorder mt="md">
         <Flex>
           <Badge size="lg" color="blue">
-            Enabled
+            {segment?.autoscrape_enabled ? "Enabled" : "Disabled"}
           </Badge>
-          <Switch color="blue" size="lg" style={{ marginLeft: "auto" }} />
+          <Switch
+            color="blue"
+            size="lg"
+            style={{ marginLeft: "auto" }}
+            onChange={toggleAutoDownload}
+            checked={segment?.autoscrape_enabled}
+          />
         </Flex>
 
         <Text fz="sm" mt="md">
-          <b>Current Progress: 55%</b> (6,831 prospects downloaded)
+          <b>Current Progress: {percentComplete}%</b> ({currentPage} /{" "}
+          {totalPages} pages scraped)
         </Text>
-        <Progress value={55} color="blue" size="xl" animate />
+        <Progress value={percentComplete} color="blue" size="xl" animate />
 
         <Flex mt="md">
           <Text size="sm" mt="6px">
             Current Page:
           </Text>
-          <NumberInput ml="auto" defaultValue={1} min={1} max={100} />
-          <Button ml="xs" size="sm" color="teal" variant="outline">
+          <NumberInput
+            ml="auto"
+            defaultValue={1}
+            min={1}
+            max={100}
+            onChange={(value: any) => setHardcodedScrapePage(value)}
+          />
+          <Button
+            ml="xs"
+            size="sm"
+            color="teal"
+            variant="outline"
+            onClick={setCurrentScrapePage}
+          >
             Save
           </Button>
         </Flex>
@@ -78,8 +167,19 @@ export default function SegmentAutodownload(props: PropsType) {
           </Text>
 
           <Flex>
-            <NumberInput defaultValue={1} min={1} max={10} />
-            <Button ml="xs" size="sm" color="teal">
+            <NumberInput
+              defaultValue={1}
+              min={1}
+              max={10}
+              onChange={(value: any) => setNumScrapesToRun(value)}
+            />
+            <Button
+              ml="xs"
+              size="sm"
+              color="teal"
+              variant="outline"
+              onClick={runScrapes}
+            >
               Run
             </Button>
           </Flex>

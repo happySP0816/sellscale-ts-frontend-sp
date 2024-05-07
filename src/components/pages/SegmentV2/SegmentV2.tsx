d4d44e07d96b3a69cf838b03a499574ddc0a76e3
happySP0816 <happydeveloper0816@outlook.com>
2024-05-06 17:39:41 -0700

    alter segments page to show accounts and calculate TAM value differently

--- src/components/pages/SegmentV2/SegmentV2.tsx
+++ src/components/pages/SegmentV2/SegmentV2.tsx
@@ -79,6 +79,7 @@ export default function SegmentV2(props: PropsType) {
   const [createSegmentParentId, setCreateSegmentParentId] = useState(null);
   const [totalProspected, setTotalProspected] = useState(0);
   const [totalContacted, setTotalContacted] = useState(0);
+  const [totalUniqueCompanies, setTotalUniqueCompanies] = useState(0);
   const [totalInFilters, setTotalInFilters] = useState(0);
   const [showAllSegments, setShowAllSegments] = useState(false);
   const [modalOpened, setModalOpened] = useState(false);
@@ -960,6 +961,10 @@ export default function SegmentV2(props: PropsType) {
           (acc: number, segment: any) => acc + (segment.num_prospected || 0),
           0
         );
+        const totalUniqueCompanies = segments.reduce(
+          (acc: number, segment: any) => acc + (segment.unique_companies || 0),
+          0
+        );
         const totalContacted = segments.reduce(
           (acc: number, segment: any) => acc + (segment.num_contacted || 0),
           0
@@ -971,6 +976,7 @@ export default function SegmentV2(props: PropsType) {
         );
         setTotalProspected(totalProspected);
         setTotalContacted(totalContacted);
+        setTotalUniqueCompanies(totalUniqueCompanies);
         setTotalInFilters(totalProspectsInPreFilters);
         const parentSegments = segments.filter(
           (segment: any) => !segment.parent_segment_id
@@ -1471,6 +1477,7 @@ export default function SegmentV2(props: PropsType) {
           totalProspected={totalProspected}
           totalContacted={totalContacted}
           totalInFilters={totalInFilters}
+          totalUniqueCompanies={totalUniqueCompanies}
         />
         <Box>
           <Flex>
--- src/components/pages/SegmentV2/SegmentV2Overview.tsx
+++ src/components/pages/SegmentV2/SegmentV2Overview.tsx
@@ -1,3 +1,4 @@
+import { userDataState } from "@atoms/userAtoms";
 import {
   ActionIcon,
   Badge,
@@ -14,15 +15,19 @@ import {
   IconDotsVertical,
   IconGlobe,
   IconMap,
+  IconCash,
+  IconBuildingBank,
   IconMessage,
   IconUsers,
 } from "@tabler/icons";
 import { IconBriefcase2 } from "@tabler/icons-react";
+import { useRecoilValue } from "recoil";
 
 export default function SegmentV2Overview(props: any) {
-  const { data, totalProspected, totalContacted, totalInFilters } = props;
+  const { data, totalProspected, totalContacted, totalInFilters, totalUniqueCompanies } = props;
   const theme = useMantineTheme();
 
+  const userData = useRecoilValue(userDataState);
   const totalSegments =
     data.length +
     data
@@ -53,8 +58,8 @@ export default function SegmentV2Overview(props: any) {
 
             <Flex align={"center"} gap={"xs"}>
               <Text fw={500} fz={"25px"}>
-                {/* # people x 10000 */}$
-                {(totalInFilters * 10000).toLocaleString()}
+                {/* # numAccounts x acv */}$
+                {(totalUniqueCompanies * userData.client.contract_size).toLocaleString()}
               </Text>
             </Flex>
           </Flex>
@@ -91,75 +96,49 @@ export default function SegmentV2Overview(props: any) {
           </Box>
         </Flex>
       </Card>
-      {/* Number segments */}
+
+      {/* ACV */}
       <Card padding="lg" radius="md" withBorder w={"15%"}>
         <Flex align={"center"} justify={"space-between"}>
           <Flex align={"center"} gap={"sm"}>
-            <IconBriefcase2
-              color="#d444f1"
+            <IconCash
+              color={theme.colors.green[3]}
               size={"1.1rem"}
               style={{ marginTop: "-1px" }}
             />
             <Text fw={500} color="gray" fz="xs">
-              # Segments
+              ACV
             </Text>
           </Flex>
         </Flex>
         <Flex align={"center"} gap={"sm"}>
           <Text size={30} fw={500}>
-            {totalSegments}
+            ${userData.client.contract_size.toLocaleString()}
           </Text>
         </Flex>
       </Card>
 
-      {/* Number in filters */}
+      {/* Accounts */}
       <Card padding="lg" radius="md" withBorder w={"15%"}>
         <Flex align={"center"} justify={"space-between"}>
           <Flex align={"center"} gap={"sm"}>
-            <IconMap
+            <IconBuildingBank
               color={theme.colors.orange[3]}
               size={"1.1rem"}
               style={{ marginTop: "-1px" }}
             />
             <Text fw={500} color="gray" fz="xs">
-              # People
+              Accounts
             </Text>
           </Flex>
         </Flex>
         <Flex align={"center"} gap={"sm"}>
           <Text size={30} fw={500}>
-            {totalInFilters.toLocaleString()}
+            {totalUniqueCompanies}
           </Text>
         </Flex>
       </Card>
-      {/* Number imported */}
-      <Card padding="lg" radius="md" withBorder w={"15%"}>
-        <Flex align={"center"} justify={"space-between"}>
-          <Flex align={"center"} gap={"sm"}>
-            <IconMessage
-              color="#40c057"
-              size={"1.1rem"}
-              style={{ marginTop: "-1px" }}
-            />
-            <Text fw={500} color="gray" fz="xs">
-              # Imported
-            </Text>
-          </Flex>
-        </Flex>
-        <Flex align={"center"} gap={"sm"}>
-          <Text size={30} fw={500}>
-            {totalProspected}
-          </Text>
-          {/* <Badge
-            color="green"
-            leftSection={
-              <IconArrowUp size={"0.9rem"} style={{ marginTop: "4px" }} />
-            }
-          >
-            8.5%
-          </Badge> */}
-        </Flex>
-      </Card>
+
 
       {/* Number contacted */}
       <Card padding="lg" radius="md" withBorder w={"15%"}>
@@ -171,13 +150,33 @@ export default function SegmentV2Overview(props: any) {
               style={{ marginTop: "-1px" }}
             />
             <Text fw={500} color="gray" fz="xs">
-              # Contacted
+              Contacts
             </Text>
           </Flex>
         </Flex>
         <Flex align={"center"} gap={"sm"}>
           <Text size={30} fw={500}>
-            {totalContacted}
+            {totalProspected}
+          </Text>
+        </Flex>
+      </Card>
+      {/* Number segments */}
+      <Card padding="lg" radius="md" withBorder w={"15%"}>
+        <Flex align={"center"} justify={"space-between"}>
+          <Flex align={"center"} gap={"sm"}>
+            <IconBriefcase2
+              color="#d444f1"
+              size={"1.1rem"}
+              style={{ marginTop: "-1px" }}
+            />
+            <Text fw={500} color="gray" fz="xs">
+              Segments
+            </Text>
+          </Flex>
+        </Flex>
+        <Flex align={"center"} gap={"sm"}>
+          <Text size={30} fw={500}>
+            {totalSegments}
           </Text>
         </Flex>
       </Card>

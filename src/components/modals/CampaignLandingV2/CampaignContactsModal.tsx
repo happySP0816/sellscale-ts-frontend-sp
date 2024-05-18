bb5c5d71e56a843100b6594dca283a64ff982f08
happySP0816 <happydeveloper0816@outlook.com>
2024-05-18 17:33:32 -0400

    Reply to make new campaign page

---
+++ src/components/modals/CampaignLandingV2/CampaignContactsModal.tsx
@@ -0,0 +1,11 @@
+import { ActionIcon, Avatar, Box, Button, Checkbox, Flex, Paper, ScrollArea, Text, Textarea } from "@mantine/core";
+import FindContactsPage from "@pages/FindContactsPage";
+import { IconBuilding, IconEdit, IconPlus, IconPoint } from "@tabler/icons";
+
+export default function CampaignContactsModal() {
+  return (
+    <>
+      <FindContactsPage />
+    </>
+  );
+}

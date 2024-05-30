22ea8ff355a9728225f903e231a6f91366623158
happySP0816 <happydeveloper0816@outlook.com>
2024-05-30 10:58:03 -0700

    campaign activates sequence channels and defaults to active false. some cleanup for that modal

--- src/components/common/persona/CreatePersona.tsx
+++ src/components/common/persona/CreatePersona.tsx
@@ -5,6 +5,7 @@ import {
   Card,
   Divider,
   Flex,
+  Paper,
   Select,
   Switch,
   Text,
@@ -42,6 +43,7 @@ export default function CreatePersona(props: PropsType) {
   const [currentProject, setCurrentProject] = useRecoilState(
     currentProjectState
   );
+  const [connectionType, setConnectionType] = useState<string | undefined>(undefined); // ["RANDOM", "ALL_PROSPECTS", "OPENED_EMAIL_PROSPECTS_ONLY", "CLICKED_LINK_PROSPECTS_ONLY"
   const navigate = useNavigate();
 
   const [emailChecked, setEmailChecked] = useState(false);
@@ -58,7 +60,7 @@ export default function CreatePersona(props: PropsType) {
     refetchOnWindowFocus: false,
   });
 
-  const createPersonaHandler = async () => {
+  const createPersonaHandler = async (linkedinChecked?: boolean, emailChecked?: boolean) => {
     setCreatingPersona(true);
     const result = await createPersona(
       userToken,
@@ -70,6 +72,9 @@ export default function CreatePersona(props: PropsType) {
         contactObjective: props.createPersona.contactObjective,
         contractSize: props.createPersona.contractSize,
         template_mode: props.createPersona.templateMode,
+        linkedinChecked,
+        emailChecked,
+        connectionType,
       }
     );
     if (result.status === "error") {
@@ -101,119 +106,131 @@ export default function CreatePersona(props: PropsType) {
     //   window.location.href = "/contacts/overview";
     // }, 3000);
 
+    //short circuit to enter the user into the campaign shell.
+    if (result.data){
+      window.location.href = `/campaign_v2/${result.data as number}`;
+      return result.data as number;
+    }
+
     window.location.reload();
 
     setCurrentProject(result.data);
+
+    
+
     return result.data as number;
   };
 
   return (
     <Card>
       <Flex direction={"column"} mb="md">
-        <Text fw={500} size={"sm"}>
-          Sequence
-        </Text>
-        <Flex align={"center"} gap={emailChecked && linkedinChecked ? 0 : "lg"}>
-          <Switch
-            miw={240}
-            labelPosition="left"
-            onChange={(e) => setEmailChecked(e.target.checked)}
-            label={
-              <Flex gap={4} align={"center"}>
-                <IconMailOpened size={"1.2rem"} fill="#3B85EF" color="white" />
-                <Text color="#3B85EF" fw={500}>
-                  Email
-                </Text>
-              </Flex>
-            }
-            w={"100%"}
-            styles={{
-              root: {
-                border: "1px solid #D9DEE5",
-                padding: "7px",
-                borderRadius: "4px",
-                background: "white",
-              },
-              body: {
-                width: "100%",
-                display: "flex",
-                justifyContent: "space-between",
-              },
-            }}
-          />
-          {emailChecked && linkedinChecked && (
-            <>
-              <Divider
-                w={"100%"}
-                variant="dashed"
-                labelPosition="center"
-                label={<Hook linkedLeft={false} linkedRight={false} />}
-              />
-              <Select
-                size="sm"
-                // value={selectedConnectionType}
-                miw={170}
-                data={[
-                  {
-                    label: "[❌] No Connection",
-                    value: "RANDOM",
-                  },
-                  {
-                    label: "[📧 → 🤝] Sent Only - ",
-                    value: "ALL_PROSPECTS",
-                  },
-                  {
-                    label: "[👀 → 🤝] Opened Only - ",
-                    value: "OPENED_EMAIL_PROSPECTS_ONLY",
-                  },
-                  {
-                    label: "[⚡️ → 🤝] Clicked Only - ",
-                    value: "CLICKED_LINK_PROSPECTS_ONLY",
-                  },
-                ]}
-                placeholder="Connect sequences"
-                withinPortal
-              />
-              <Divider
-                w={"100%"}
-                variant="dashed"
-                labelPosition="center"
-                label={<Hook linkedLeft={false} linkedRight={false} />}
-              />
-            </>
-          )}
-          <Switch
-            labelPosition="left"
-            onChange={(e) => setLinkedinChecked(e.target.checked)}
-            label={
-              <Flex gap={4} align={"center"}>
-                <IconBrandLinkedin
-                  size={"1.4rem"}
-                  fill="#3B85EF"
-                  color="white"
+        <Paper shadow="sm" p="md" withBorder>
+          <Text mb="lg" fw={500} size={"sm"} align="center">
+            Sequence
+          </Text>
+          <Flex align={"center"} gap={emailChecked && linkedinChecked ? 0 : "lg"}>
+            <Switch
+              miw={200}
+              labelPosition="left"
+              onChange={(e) => setEmailChecked(e.target.checked)}
+              label={
+                <Flex gap={4} align={"center"}>
+                  <IconMailOpened size={"1.2rem"} fill="#3B85EF" color="white" />
+                  <Text color="#3B85EF" fw={500}>
+                    Email
+                  </Text>
+                </Flex>
+              }
+              w={"100%"}
+              styles={{
+                root: {
+                  border: "1px solid #D9DEE5",
+                  padding: "7px",
+                  borderRadius: "4px",
+                  background: "white",
+                },
+                body: {
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "space-between",
+                },
+              }}
+            />
+            {emailChecked && linkedinChecked && (
+              <>
+                <Divider
+                  w={"100%"}
+                  variant="dashed"
+                  labelPosition="center"
+                  label={<Hook linkedLeft={false} linkedRight={false} />}
                 />
-                <Text color="#3B85EF" fw={500}>
-                  Linkedin
-                </Text>
-              </Flex>
-            }
-            miw={240}
-            w={"100%"}
-            styles={{
-              root: {
-                border: "1px solid #D9DEE5",
-                padding: "7px",
-                borderRadius: "4px",
-                background: "white",
-              },
-              body: {
-                width: "100%",
-                display: "flex",
-                justifyContent: "space-between",
-              },
-            }}
-          />
-        </Flex>
+                <Select
+                  size="sm"
+                  miw={170}
+                  value={connectionType}
+                  data={[
+                    {
+                      label: "[❌] No Connection",
+                      value: "RANDOM",
+                    },
+                    {
+                      label: "[📧 → 🤝] Sent Only - ",
+                      value: "ALL_PROSPECTS",
+                    },
+                    {
+                      label: "[👀 → 🤝] Opened Only - ",
+                      value: "OPENED_EMAIL_PROSPECTS_ONLY",
+                    },
+                    {
+                      label: "[⚡️ → 🤝] Clicked Only - ",
+                      value: "CLICKED_LINK_PROSPECTS_ONLY",
+                    },
+                  ]}
+                  placeholder="Connect sequences"
+                  withinPortal
+                  onChange={(value) => setConnectionType(value)}
+                />
+                <Divider
+                  w={"100%"}
+                  variant="dashed"
+                  labelPosition="center"
+                  label={<Hook linkedLeft={false} linkedRight={false} />}
+                />
+              </>
+            )}
+            <Switch
+              labelPosition="left"
+              onChange={(e) => setLinkedinChecked(e.target.checked)}
+              label={
+                <Flex gap={4} align={"center"}>
+                  <IconBrandLinkedin
+                    size={"1.4rem"}
+                    fill="#3B85EF"
+                    color="white"
+                  />
+                  <Text color="#3B85EF" fw={500}>
+                    Linkedin
+                  </Text>
+                </Flex>
+              }
+              miw={240}
+              w={"100%"}
+              styles={{
+                root: {
+                  border: "1px solid #D9DEE5",
+                  padding: "7px",
+                  borderRadius: "4px",
+                  background: "white",
+                },
+                body: {
+                  width: "100%",
+                  display: "flex",
+                  justifyContent: "space-between",
+                },
+              }}
+            />
+          </Flex>
+        </Paper>
       </Flex>
       <Flex align={"center"} justify={"space-between"} gap={"lg"}>
         <Button
@@ -228,7 +245,7 @@ export default function CreatePersona(props: PropsType) {
         </Button>
         <Button
           // disabled={!props.createPersona?.name || !props.createPersona.contactObjective}
-          onClick={() => createPersonaHandler()}
+          onClick={() => createPersonaHandler(linkedinChecked, emailChecked)}
           loading={creatingPersona}
           fullWidth
         >

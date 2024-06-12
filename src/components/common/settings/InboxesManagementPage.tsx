e110869f9d4e9e4dcb4b8c070494ceb48212f76b
happySP0816 <happydeveloper0816@outlook.com>
2024-06-12 18:56:01 -0400

    Reply to add spam checker modal

--- src/components/common/settings/InboxesManagementPage.tsx
+++ src/components/common/settings/InboxesManagementPage.tsx
@@ -1,14 +1,4 @@
-import {
-  Button,
-  Card,
-  Flex,
-  Group,
-  Modal,
-  NumberInput,
-  Paper,
-  Text,
-  Title,
-} from "@mantine/core";
+import { Button, Card, Flex, Group, Modal, NumberInput, Paper, Select, Text, TextInput, Title } from "@mantine/core";
 import MultiEmails from "./MultiEmails/MultiEmails";
 import { useDisclosure } from "@mantine/hooks";
 import { useForm } from "@mantine/form";
@@ -18,9 +8,11 @@ import { API_URL } from "@constants/data";
 import { useRecoilValue } from "recoil";
 import { userTokenState } from "@atoms/userAtoms";
 import { showNotification } from "@mantine/notifications";
+import { IconTrash } from "@tabler/icons";
 
 export const InboxesManagementPage = () => {
   const [opened, { open, close }] = useDisclosure(false);
+  const [openedSpamModal, { open: openSpamModal, close: closeSpamModal }] = useDisclosure(false);
 
   return (
     <Paper withBorder>
@@ -30,13 +22,16 @@ export const InboxesManagementPage = () => {
             <Text fw={600} size={24}>
               Inbox Management
             </Text>
-            <Button onClick={open}>Request More Inboxes</Button>
+            <Flex gap={"md"}>
+              <Button color="yellow" variant="outline" onClick={openSpamModal}>
+                Spam Checker
+              </Button>
+              <Button onClick={open}>Request More Inboxes</Button>
+            </Flex>
             <InboxRequestModal opened={opened} close={close} />
+            <SpamCheckerModal opened={openedSpamModal} close={closeSpamModal} />
           </Flex>
-          <Text>
-            SellScale provides multiple managed domains and inboxes in order to
-            meet your outbounding needs. Review your inboxes and domains below.
-          </Text>
+          <Text>SellScale provides multiple managed domains and inboxes in order to meet your outbounding needs. Review your inboxes and domains below.</Text>
         </Flex>
 
         <Flex mt="sm">
@@ -60,19 +55,16 @@ const InboxRequestModal = (props: { opened: boolean; close: () => void }) => {
   const submitInboxRequest = async (numberInboxes: number) => {
     setIsSubmitting(true);
 
-    fetch(
-      `${API_URL}/domains/inboxes/request`,
-      {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${userToken}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          number_inboxes: numberInboxes,
-        }),
-      }
-    ).then((response) => {
+    fetch(`${API_URL}/domains/inboxes/request`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        number_inboxes: numberInboxes,
+      }),
+    }).then((response) => {
       if (response.ok) {
         showNotification({
           title: "Request Submitted",
@@ -103,37 +95,22 @@ const InboxRequestModal = (props: { opened: boolean; close: () => void }) => {
       title={<Title order={3}>Request More Inboxes</Title>}
     >
       <Text>
-        We are happy to provide more domains and inboxes for you to outbound
-        from. Please fill out this short request form and we will contact you
-        shortly.
+        We are happy to provide more domains and inboxes for you to outbound from. Please fill out this short request form and we will contact you shortly.
       </Text>
       <form
         onSubmit={form.onSubmit((values) => {
           submitInboxRequest(values.numberInboxes);
         })}
       >
-        <NumberInput
-          mt="md"
-          label="Number of Inboxes"
-          placeholder="Number of Inboxes"
-          min={0}
-          required
-          {...form.getInputProps("numberInboxes")}
-        />
+        <NumberInput mt="md" label="Number of Inboxes" placeholder="Number of Inboxes" min={0} required {...form.getInputProps("numberInboxes")} />
 
-        {(form.getInputProps("numberInboxes").value > 0 ||
-          form.isTouched()) && (
+        {(form.getInputProps("numberInboxes").value > 0 || form.isTouched()) && (
           <>
             <Text mt="md">
-              <span style={{ fontWeight: "500", fontSize: "15px" }}>
-                Estimated Volume:
-              </span>{" "}
-              {form.getInputProps("numberInboxes").value * 5 * 30} emails / week
+              <span style={{ fontWeight: "500", fontSize: "15px" }}>Estimated Volume:</span> {form.getInputProps("numberInboxes").value * 5 * 30} emails / week
             </Text>
             <Text fz="xs">
-              {`${
-                form.getInputProps("numberInboxes").value || 0
-              } inboxes\n x 30 emails per inbox per day x 5 days per week = ${
+              {`${form.getInputProps("numberInboxes").value || 0} inboxes\n x 30 emails per inbox per day x 5 days per week = ${
                 form.getInputProps("numberInboxes").value * 5 * 30
               } emails per week`}
             </Text>
@@ -141,15 +118,44 @@ const InboxRequestModal = (props: { opened: boolean; close: () => void }) => {
         )}
 
         <Group position="right" mt="md">
-          <Button
-            type="submit"
-            disabled={form.getInputProps("numberInboxes").value < 1}
-            loading={isSubmitting}
-          >
+          <Button type="submit" disabled={form.getInputProps("numberInboxes").value < 1} loading={isSubmitting}>
             Request
           </Button>
         </Group>
       </form>
+    </Modal>
+  );
+};
+
+const SpamCheckerModal = (props: { opened: boolean; close: () => void }) => {
+  return (
+    <Modal
+      title={
+        <Flex align={"center"} gap={"sm"}>
+          <IconTrash color="orange" size={"1.4rem"} className="mb-[3px]" />
+          <Title order={3}>Spam Checker</Title>
+        </Flex>
+      }
+      opened={props.opened}
+      onClose={() => {
+        props.close();
+      }}
+    >
+      <Paper>
+        <Text color="gray" size={"sm"}>
+          Check if your emails are landing in spam.
+        </Text>
+        <Select data={[""]} placeholder="Select email template" label="Choose a recent email template:" mt={"lg"} />
+        <TextInput placeholder="Enter email ID" label="Send email to:" mt={"lg"} />
+        <Flex gap={"md"} mt={40}>
+          <Button fullWidth variant="outline" color="gray">
+            Cancel
+          </Button>
+          <Button fullWidth onClick={() => alert("Email sent to <Insert Email Here>!")}>
+            Send Spam Check Email
+          </Button>
+        </Flex>
+      </Paper>
     </Modal>
   );
 };

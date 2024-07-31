cd6086971216306a2e7737b91ca3e2b88b2b9dd1
happySP0816 <happydeveloper0816@outlook.com>
2024-07-31 10:22:15 -0700

    top icp people in analytics

--- src/components/pages/AIBrain/AnalyticsItem.tsx
+++ src/components/pages/AIBrain/AnalyticsItem.tsx
@@ -456,7 +456,7 @@ const AnalyticsItem = ({ dailyData, templateAnalytics, topIcpPeople, summaryData
               )}
             </Box>
           </Flex>
-          {/* thereExistsICPData && <Paper>
+          { thereExistsICPData && <Paper>
             <Flex align={"center"} gap={"5px"}>
               <Text
                 style={{
@@ -511,12 +511,27 @@ const AnalyticsItem = ({ dailyData, templateAnalytics, topIcpPeople, summaryData
                                       icpItem.icp_fit_score == 4 ? "Very High" : "Not Scored"
                             }</Badge>
                         </Flex>
+                        <Badge
+                                tt={"initial"}
+                                variant="filled"
+                                rightSection={<IconExternalLink size={"0.9rem"} style={{ marginTop: "5px" }} />}
+                                styles={{
+                                  root: {
+                                    fontWeight: 400,
+                                  },
+                                }}
+                                component="a"
+                                href={`/prospects/${icpItem.id}`}
+                                target="_blank"
+                              >
+                                View Conversation
+                              </Badge>
                       </Flex>
                     </Paper>
                   );
                 })}
             </SimpleGrid>
-          </Paper> */}
+          </Paper> }
         </Flex>
       ) : (
        <></>

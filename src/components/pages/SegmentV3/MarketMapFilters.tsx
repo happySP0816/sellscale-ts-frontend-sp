d0d8d2992fd18475ab27be2456b329a766685fdb
happySP0816 <happydeveloper0816@outlook.com>
2024-08-07 12:57:05 -0700

    hotfix

--- src/components/pages/SegmentV3/MarketMapFilters.tsx
+++ src/components/pages/SegmentV3/MarketMapFilters.tsx
@@ -1359,6 +1359,8 @@ const MarketMapFilters = function (
                         color={'red'}
                         onClick={() => {
                           setContactTableHeaders(prevState => prevState.filter(item => item.key !== aiFilter.key));
+                          setDealBreakers(prevState => prevState.filter(x => x !== aiFilter.key));
+                          setIndividualPersonalizers(prevState => prevState.filter(x => x !== aiFilter.key));
                           setIndividualAIFilters(prevState => prevState.filter(item => item.key !== aiFilter.key))
                         }}>
                         Delete
@@ -2058,6 +2060,8 @@ const MarketMapFilters = function (
                         color={'red'}
                         onClick={() => {
                           setCompanyTableHeaders(prevState => prevState.filter(item => item.key !== aiFilter.key));
+                          setDealBreakers(prevState => prevState.filter(x => x !== aiFilter.key));
+                          setCompanyPersonalizers(prevState => prevState.filter(x => x !== aiFilter.key));
                           setCompanyAIFilters(prevState => prevState.filter(item => item.key !== aiFilter.key))
                         }}>
                         Delete
